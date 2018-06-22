472dc2b9022594ba636dfafe89f6fa2ff0e4968c
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-06-22 09:53:09 -0500

    Follow along with lesson 8.

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,29 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+/*
+Note from the Wes Bos course - Stateless Functional Component
+These components carry no state, and thus, can be set up as a function that
+accepts a single parameter of props.
+
+You can take things a step further, as illustrated here, and instead of
+passing in props and saying, say, "props.tagline" where you use it, you can
+instead use ES6 object destructuring to grab all the keys of the props object,
+then you can just call that key. So, in this case, we have just tagline.
+ */
+const Header = ({ tagline }) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
 
 export default Header;
